f82bacb6f00e46f2b4fb43c82fc04152a0b910b6
ARR2005 <Aldwinrich90@gmail.com>
2026-02-19 20:36:39 +0800

    created: controller w/ list of products

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    public function products()
+    {
+        $products = [
+        'Naruto',
+        'One Piece',
+        'Attack on Titan',
+        'My Hero Academia',
+        'Demon Slayer',
+        'Boku no Hero Academia',
+        'Dragon Ball Z',
+        'Fullmetal Alchemist',
+        'Death Note',
+        'Sword Art Online'
+        ];
+
+        return view('anime.index', ['products' => $products]);
+    }
+
+}
